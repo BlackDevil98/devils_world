b05daa0fd5ed9df1d63008d6c9159738c311a07e
blackdevil <zinzuvadiyameet98@gmail.com>
2019-10-10 14:20:32 +0530

    Add navbar in nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { NavComponent } from './nav/nav.component';
+
+const routes: Routes = [
+  { path: 'nav', component: NavComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
